a178cded5cc4a5d58f90205ba50a81bbfae0e715
Ricardo Luna <r_luna99@hotmail.com>
2020-05-07 12:39:10 -0600

    New app created and Bootstrap added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import Alert from 'react-bootstrap/Alert'
+import 'bootstrap/dist/css/bootstrap.css'
+
+class App extends Component {
+    render() {
+        return (
+            <Alert key='primary' variant='primary'>HI</Alert>
+        )
+    }
+}
+
+export default App
