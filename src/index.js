6ad2017c06269efbe7264859e0aa4b897d6ae09e
Ricardo Luna <r_luna99@hotmail.com>
2020-05-07 13:24:19 -0600

    Navbar added but not modified

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import Barra from './Barra'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<Barra />, document.getElementById('root'))
